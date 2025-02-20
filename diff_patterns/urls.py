fcc0c5f77826a6372d41fa5088968b56190383de
mohan277 <senammohanakrishna@gmail.com>
2025-02-20 15:13:08 +0530

    added edit page

--- diff_patterns/urls.py
+++ diff_patterns/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from .views import CreateKnownDiffAPIView, ListKnownDiffAPIView, \
-    DetailKnownDiffAPIView
+    DetailKnownDiffAPIView, EditKnownDiffAPIView
 
 
 urlpatterns = [
     path('list-known-diff/', ListKnownDiffAPIView.as_view(), name='list_known_diff'),
     path('create-known-diff/', CreateKnownDiffAPIView.as_view(), name='create_known_diff'),
     path('known-diff/<int:pk>/', DetailKnownDiffAPIView.as_view(), name='detail_known_diff'),
-    # path('edit-recipe/<int:pk>/', EditRecipeAPIView.as_view(), name='edit_recipe'),
+    path('edit-known-diff/<int:pk>/', EditKnownDiffAPIView.as_view(), name='edit_known_diff'),
     # path("delete-recipe/<int:pk>/", DeleteRecipeAPIView.as_view(), name="delete_recipe"),
 
 ]
--- diff_patterns/views.py
+++ diff_patterns/views.py
@@ -70,25 +70,58 @@ class CreateKnownDiffAPIView(APIView):
             return render(request, self.template_name, {'known_diff_form': form})
 
 
-# class EditRecipeAPIView(APIView):
-#     permission_classes = [UserAuthentication]
-#     template_name = 'edit_recipe.html'
+class EditKnownDiffAPIView(APIView):
+    permission_classes = [UserAuthentication]
+    template_name = 'edit_known_diff.html'
 
-#     def get(self, request, pk):
-#         form = CreateRecipeModalForm()
-#         recipe_obj = Recipe.objects.filter(id=pk).first()
-#         return render(request, self.template_name, {'recipe_form': form, "recipe_obj": recipe_obj})
+    def get(self, request, pk):
+        form = CreateKnownDiffModalForm()
+        known_diff = KnownDiff.objects.filter(
+            ~Q(is_active=3), id=pk
+        ).first()
 
-#     def post(self, request, pk):
-#         form = CreateRecipeModalForm(request.POST)
-#         if form.is_valid():
-#             form_data = form.cleaned_data
-#             Recipe.objects.filter(id=pk).update(
-#                 **form_data, updated_at=datetime.datetime.now()
-#             )
-#             return redirect('list_recipe')
-#         else:
-#             return render(request, self.template_name, {'recipe_form': form})
+        known_diff_details = {}
+        if known_diff:
+            description = json.loads(known_diff.description) if known_diff.description else {}
+            known_diff_details.update({
+                **description,
+                "id": known_diff.id,
+                "diff_name": known_diff.diff_name,
+                "diff_url": known_diff.diff_url,
+                "rule_id": known_diff.rule_id,
+                "raised_by": known_diff.raised_by,
+                "assigned_to": known_diff.assigned_to,
+                "raised_by_name": known_diff.raised_by.username if known_diff.raised_by else "",
+                "assigned_to_name": known_diff.assigned_to.username if known_diff.assigned_to else ""
+            })
+
+        return render(request, self.template_name, {'known_diff_form': form, "known_diff_details": known_diff_details})
+
+    def post(self, request, pk):
+        form = CreateKnownDiffModalForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            description = json.dumps({
+                "issue_description": form_data.get("issue_description"),
+                "behaviour1": form_data.get("behaviour1"),
+                "behaviour2": form_data.get("behaviour2"),
+                "fb": form_data.get("fb"),
+                "model_mapping": form_data.get("model_mapping")
+            })
+
+            # creating Known Diff
+            KnownDiff.objects.filter(id=pk).update(
+                diff_name=form_data.get("diff_name"),
+                rule_id=form_data.get("rule_id"),
+                diff_url=form_data.get("diff_url"),
+                raised_by=form_data.get("raised_by"),
+                assigned_to=form_data.get("assigned_to"),
+                description=description,
+                updated_at=datetime.datetime.now()
+            )
+            return redirect('detail_known_diff', pk=pk)
+        else:
+            return render(request, self.template_name, {'known_diff_form': form})
 
 
 class DetailKnownDiffAPIView(APIView):
@@ -106,6 +139,7 @@ class DetailKnownDiffAPIView(APIView):
             description = json.loads(known_diff.description) if known_diff.description else {}
             known_diff_details.update({
                 **description,
+                "id": known_diff.id,
                 "diff_name": known_diff.diff_name,
                 "diff_url": known_diff.diff_url,
                 "rule_id": known_diff.rule_id,
